84085296e90f916db2f312316a4abdfa179cf24b
tono <ristono@salt.co.id>
2021-03-11 18:05:06 +0700

    add param in say hello

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/ristono404/go-say-hello
+module github.com/ristono404/go-say-hello/v2
 
 go 1.16
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hallo guys"
+func SayHello(name string) string {
+	return "Hallo " + name
 }
